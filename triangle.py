7fbc48eb171e4f6994f77124ef7e5c158bcf7917
May <53219665+Myy0508@users.noreply.github.com>
2019-07-28 10:44:36 +0800

    Delete triangle.py

--- triangle.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-list2d = [[1,2,3],[4,5,6]]
-sum = 0
-for i in range(len(list2d)):
-    for j in range(len(list2d[0])):
-        sum += list2d[i][j]
-print(sum)
